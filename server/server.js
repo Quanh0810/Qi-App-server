68ea3aa845c3e5a67155efb38d0e25f1c43d411a
Quanh0810 <nqunhanh35@gmail.com>
2023-03-14 22:54:26 +0700

    client up

--- server/server.js
+++ server/server.js
@@ -6,9 +6,11 @@ const dbConfig = require('./config/dbConfig');
 
 const usersRoute = require("./routes/userRoute");
 const examsRoute = require("./routes/examsRoute");
+const reportsRoute = require("./routes/reportsRoute");
 
 app.use("/api/users", usersRoute);
 app.use("/api/exams", examsRoute);
+app.use("/api/reports", reportsRoute);
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
