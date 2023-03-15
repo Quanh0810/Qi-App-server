10d2af8a6924cc36171f5a21f9cb5e66bc944ce6
Quanh0810 <nqunhanh35@gmail.com>
2023-03-15 23:54:20 +0700

    server up

--- server/routes/reportsRoute.js
+++ server/routes/reportsRoute.js
@@ -27,7 +27,32 @@ router.post("/add-report", authMiddleware, async (req, res) => {
 
 router.post("/get-all-reports", authMiddleware, async (req, res) => {
   try {
-    const reports = await Report.find()
+    const { examName, userName } = req.body;
+
+    const exams = await Exam.find({
+      name: {
+        $regex: examName,
+      },
+    });
+
+    const matchedExamIds = exams.map((exam) => exam._id);
+
+    const users = await User.find({
+      name: {
+        $regex: userName,
+      },
+    });
+
+    const matchedUserIds = users.map((user) => user._id);
+
+    const reports = await Report.find({
+      exam: {
+        $in: matchedExamIds,
+      },
+      // user: {
+      //   $in: matchedUserIds,
+      // },
+    })
       .populate("exam")
       .populate("user")
       .sort({ createdAt: -1 });
